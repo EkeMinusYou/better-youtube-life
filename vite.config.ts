9c813861e29d7eae031f20476f0df2475a237d2b
EkeMinusYou <euonymuslke@gmail.com>
2023-12-20 23:25:26 +0900

    install vite chrome plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
 
-// https://vitejs.dev/config/
+const manifest = defineManifest({
+	manifest_version: 3,
+	name: "Shortcut for Youtube",
+	version: "1.0.0",
+	content_scripts: [
+		{
+			matches: ["https://www.google.com/*"],
+			js: ["src/contentScript/script.ts"],
+		},
+	],
+});
+
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), crx({ manifest })],
+});
