f9411c708b899d24384918b30c58ab002a5eb87b
EkeMinusYou <euonymuslke@gmail.com>
2023-12-23 16:29:30 +0900

    add contentScript

---
+++ src/contentScript/script.ts
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", (_event) => {
+	const targetElement = document.querySelector(".target-class");
+	if (targetElement) {
+		targetElement.classList.add("custom-styled");
+	}
+});
