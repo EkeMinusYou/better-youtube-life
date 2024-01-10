14b8b18041a5b72375c45cdb43c7045e39b59696
EkeMinusYou <euonymuslke@gmail.com>
2024-01-11 03:14:49 +0900

    rename content path

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const manifest = defineManifest({
 	content_scripts: [
 		{
 			matches: ["https://www.youtube.com/*"],
-			js: ["src/contentScript/script.ts"],
+			js: ["src/content/index.ts"],
 		},
 	],
 });
