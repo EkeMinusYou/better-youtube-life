997c71324ed60180dbfb071168735c49ddf97f92
EkeMinusYou <euonymuslke@gmail.com>
2024-01-14 17:18:52 +0900

    rename tsx

--- src/content/index.ts
+++
@@ -1,6 +0,0 @@
-window.addEventListener("DOMContentLoaded", (_event) => {
-	const targetElement = document.querySelector(".target-class");
-	if (targetElement) {
-		targetElement.classList.add("custom-styled");
-	}
-});
---
+++ src/content/index.tsx
@@ -0,0 +1,4 @@
+window.addEventListener("DOMContentLoaded", (_event) => {
+	const targetElement = document.getElementById("search-input");
+	targetElement?.click();
+});
