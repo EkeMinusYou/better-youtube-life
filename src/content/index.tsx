70ebd09902c0da0dc675294d8949968643ce4b57
EkeMinusYou <euonymuslke@gmail.com>
2024-05-16 04:38:46 +0900

    wip

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,10 +1,8 @@
 import "./index.css";
 
 document.addEventListener("keydown", function (event) {
-  if (event.key === "Shift") {
-    let videos = document.querySelectorAll("ytd-thumbnail");
-    videos.forEach(function (video) {
-      video.classList.toggle("highlighted");
-    });
+  if (event.key === "k") {
+    const videos = document.querySelectorAll("ytd-thumbnail");
+    videos[1].classList.toggle("highlighted");
   }
 });
