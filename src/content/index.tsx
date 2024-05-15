34f12f3acaa7202637de34dccb15c72bfb7594fe
EkeMinusYou <euonymuslke@gmail.com>
2024-05-16 04:43:46 +0900

    fix name

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,8 +1,12 @@
 import "./index.css";
 
+const classes = {
+  highlighted: "sfc-highlighted",
+};
+
 document.addEventListener("keydown", function (event) {
   if (event.key === "k") {
     const videos = document.querySelectorAll("ytd-thumbnail");
-    videos[1].classList.toggle("highlighted");
+    videos[1].classList.toggle(classes.highlighted);
   }
 });
