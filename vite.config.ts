b1897fe1d30bc75ab8e3458fbe520188cb60833e
EkeMinusYou <euonymuslke@gmail.com>
2024-05-15 22:51:47 +0900

    format

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,18 @@ import react from "@vitejs/plugin-react";
 import { crx, defineManifest } from "@crxjs/vite-plugin";
 
 const manifest = defineManifest({
-	manifest_version: 3,
-	name: "Shortcuts for Youtube",
-	version: "0.0.1",
-	content_scripts: [
-		{
-			matches: ["https://www.youtube.com/*"],
-			js: ["src/content/index.tsx"],
-		},
-	],
+  manifest_version: 3,
+  name: "Shortcuts for Youtube",
+  version: "0.0.1",
+  content_scripts: [
+    {
+      matches: ["https://www.youtube.com/*"],
+      js: ["src/content/index.tsx"],
+      css: ["src/content/index.css"],
+    },
+  ],
 });
 
 export default defineConfig({
-	plugins: [react(), crx({ manifest })],
+  plugins: [react(), crx({ manifest })],
 });
