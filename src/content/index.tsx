dcb08e6aa74310644ebc77ae4da9368704e3c0b2
EkeMinusYou <euonymuslke@gmail.com>
2024-05-16 04:01:00 +0900

    Revert "install @vanilla-extarct"
    This reverts commit 2558a58692c5a54b6f06de55469ca61e985fb762.

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,10 +1,10 @@
-import { highlighted } from "./styles.css";
+import "./index.css";
 
 document.addEventListener("keydown", function (event) {
   if (event.key === "Shift") {
     let videos = document.querySelectorAll("ytd-thumbnail");
     videos.forEach(function (video) {
-      video.classList.toggle(highlighted);
+      video.classList.toggle("highlighted");
     });
   }
 });
--- src/content/styles.css.ts
+++
@@ -1,6 +0,0 @@
-import { style } from "@vanilla-extract/css";
-
-export const highlighted = style({
-  border: "4px solid red",
-  boxSizing: "border-box",
-});
