c5e7dd3d22103b5e8e88e162b5a5dc98543cc7ff
EkeMinusYou <euonymuslke@gmail.com>
2024-05-16 23:41:30 +0900

    emotion

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,12 +1,17 @@
-import "./index.css";
+import { css } from "@emotion/css";
 
-const classes = {
-  highlighted: "sfc-highlighted",
-};
+const style = css({
+  border: "4px solid red",
+  boxSizing: "border-box",
+});
 
 document.addEventListener("keydown", function (event) {
   if (event.key === "k") {
     const videos = document.querySelectorAll("ytd-thumbnail");
-    videos[1].classList.toggle(classes.highlighted);
+    videos[1].classList.add(style);
+  }
+  if (event.key === "j") {
+    const videos = document.querySelectorAll("ytd-thumbnail");
+    videos[1].classList.remove(style);
   }
 });
