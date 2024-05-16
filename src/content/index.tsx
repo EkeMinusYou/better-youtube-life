7bdb133fcf391211e72e23015510150873bd37de
EkeMinusYou <euonymuslke@gmail.com>
2024-05-17 01:55:42 +0900

    wip

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -6,12 +6,51 @@ const style = css({
 });
 
 document.addEventListener("keydown", function (event) {
-  if (event.key === "k") {
+  // 0は初期値として扱う。0はどの動画も示さない。
+  let current: number = 0;
+
+  if (event.key === "h") {
     const videos = document.querySelectorAll("ytd-thumbnail");
-    videos[1].classList.add(style);
+
+    if (current === 0) {
+      current = 1;
+    } else {
+      videos[current].classList.remove(style);
+      current--;
+    }
+    videos[current].classList.add(style);
   }
+
   if (event.key === "j") {
     const videos = document.querySelectorAll("ytd-thumbnail");
-    videos[1].classList.remove(style);
+    if (current === 0) {
+      current = 1;
+    } else {
+      videos[current].classList.remove(style);
+      // TODO: implement
+    }
+    videos[current].classList.add(style);
+  }
+
+  if (event.key === "k") {
+    const videos = document.querySelectorAll("ytd-thumbnail");
+    if (current === 0) {
+      current = 1;
+    } else {
+      videos[current].classList.remove(style);
+      // TODO: implement
+    }
+    videos[current].classList.add(style);
+  }
+
+  if (event.key === "l") {
+    const videos = document.querySelectorAll("ytd-thumbnail");
+    if (current === 0) {
+      current = 1;
+    } else {
+      videos[current].classList.remove(style);
+      current++;
+    }
+    videos[current].classList.add(style);
   }
 });
