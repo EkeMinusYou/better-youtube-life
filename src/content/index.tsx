1b3234c804c95f3e916aabd739672b8d78ec68bd
EkeMinusYou <euonymuslke@gmail.com>
2024-05-18 03:31:49 +0900

    impl left right

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,18 +1,20 @@
 import { keymaps } from "./keymaps";
 import { styles } from "./styles";
 
+let current = 0;
+
 document.addEventListener("keydown", function (event) {
   // 0はどの動画も示さない。
   switch (event.key) {
     case keymaps.Left:
       handleKeyLeft();
       break;
-    case keymaps.Down:
-      handleKeyDown();
-      break;
-    case keymaps.Up:
-      handlekeyUp();
-      break;
+    // case keymaps.Down:
+    //   handleKeyDown();
+    //   break;
+    // case keymaps.Up:
+    //   handlekeyUp();
+    //   break;
     case keymaps.Right:
       handleKeyRight();
       break;
@@ -20,7 +22,6 @@ document.addEventListener("keydown", function (event) {
 });
 
 const handleKeyLeft = () => {
-  let current = 0;
   const videos = document.querySelectorAll("ytd-thumbnail");
 
   if (current === 0) {
@@ -32,36 +33,35 @@ const handleKeyLeft = () => {
   videos[current].classList.add(styles.highlight);
 };
 
-const handleKeyDown = () => {
-  let current = 0;
-
-  const videos = document.querySelectorAll("ytd-thumbnail");
-  if (current === 0) {
-    current = 1;
-  } else {
-    videos[current].classList.remove(styles.highlight);
-    // TODO: implement
-  }
-  videos[current].classList.add(styles.highlight);
-};
-
-const handlekeyUp = () => {
-  let current = 0;
-
-  const videos = document.querySelectorAll("ytd-thumbnail");
-  if (current === 0) {
-    current = 1;
-  } else {
-    videos[current].classList.remove(styles.highlight);
-    // TODO: implement
-  }
-  videos[current].classList.add(styles.highlight);
-};
+// const handleKeyDown = () => {
+//   let current = 0;
+//
+//   const videos = document.querySelectorAll("ytd-thumbnail");
+//   if (current === 0) {
+//     current = 1;
+//   } else {
+//     videos[current].classList.remove(styles.highlight);
+//     // TODO: implement
+//   }
+//   videos[current].classList.add(styles.highlight);
+// };
+//
+// const handlekeyUp = () => {
+//   let current = 0;
+//
+//   const videos = document.querySelectorAll("ytd-thumbnail");
+//   if (current === 0) {
+//     current = 1;
+//   } else {
+//     videos[current].classList.remove(styles.highlight);
+//     // TODO: implement
+//   }
+//   videos[current].classList.add(styles.highlight);
+// };
 
 const handleKeyRight = () => {
-  let current = 0;
-
   const videos = document.querySelectorAll("ytd-thumbnail");
+
   if (current === 0) {
     current = 1;
   } else {
