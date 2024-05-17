1cd6254ef77dc42ebb079ae868f7dbf50b84e5bb
EkeMinusYou <euonymuslke@gmail.com>
2024-05-17 23:08:07 +0900

    reafactor

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -2,51 +2,71 @@ import { keymaps } from "./keymaps";
 import { styles } from "./styles";
 
 document.addEventListener("keydown", function (event) {
-  // 0は初期値として扱う。0はどの動画も示さない。
-  let current: number = 0;
-
-  if (event.key === keymaps.Left) {
-    const videos = document.querySelectorAll("ytd-thumbnail");
-
-    if (current === 0) {
-      current = 1;
-    } else {
-      videos[current].classList.remove(styles.highlight);
-      current--;
-    }
-    videos[current].classList.add(styles.highlight);
-  }
-
-  if (event.key === keymaps.Down) {
-    const videos = document.querySelectorAll("ytd-thumbnail");
-    if (current === 0) {
-      current = 1;
-    } else {
-      videos[current].classList.remove(styles.highlight);
-      // TODO: implement
-    }
-    videos[current].classList.add(styles.highlight);
-  }
-
-  if (event.key === keymaps.Up) {
-    const videos = document.querySelectorAll("ytd-thumbnail");
-    if (current === 0) {
-      current = 1;
-    } else {
-      videos[current].classList.remove(styles.highlight);
-      // TODO: implement
-    }
-    videos[current].classList.add(styles.highlight);
-  }
-
-  if (event.key === keymaps.Right) {
-    const videos = document.querySelectorAll("ytd-thumbnail");
-    if (current === 0) {
-      current = 1;
-    } else {
-      videos[current].classList.remove(styles.highlight);
-      current++;
-    }
-    videos[current].classList.add(styles.highlight);
+  // 0はどの動画も示さない。
+  switch (event.key) {
+    case keymaps.Left:
+      handleKeyLeft();
+      break;
+    case keymaps.Down:
+      handleKeyDown();
+      break;
+    case keymaps.Up:
+      handlekeyUp();
+      break;
+    case keymaps.Right:
+      handleKeyRight();
+      break;
   }
 });
+
+const handleKeyLeft = () => {
+  let current = 0;
+  const videos = document.querySelectorAll("ytd-thumbnail");
+
+  if (current === 0) {
+    current = 1;
+  } else {
+    videos[current].classList.remove(styles.highlight);
+    current--;
+  }
+  videos[current].classList.add(styles.highlight);
+};
+
+const handleKeyDown = () => {
+  let current = 0;
+
+  const videos = document.querySelectorAll("ytd-thumbnail");
+  if (current === 0) {
+    current = 1;
+  } else {
+    videos[current].classList.remove(styles.highlight);
+    // TODO: implement
+  }
+  videos[current].classList.add(styles.highlight);
+};
+
+const handlekeyUp = () => {
+  let current = 0;
+
+  const videos = document.querySelectorAll("ytd-thumbnail");
+  if (current === 0) {
+    current = 1;
+  } else {
+    videos[current].classList.remove(styles.highlight);
+    // TODO: implement
+  }
+  videos[current].classList.add(styles.highlight);
+};
+
+const handleKeyRight = () => {
+  let current = 0;
+
+  const videos = document.querySelectorAll("ytd-thumbnail");
+  if (current === 0) {
+    current = 1;
+  } else {
+    videos[current].classList.remove(styles.highlight);
+    current++;
+  }
+  videos[current].classList.add(styles.highlight);
+};
