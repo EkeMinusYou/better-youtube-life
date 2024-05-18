b3a799310872593a9b647ab0773601fecdc9e1c7
EkeMinusYou <euonymuslke@gmail.com>
2024-05-19 00:21:23 +0900

    enable when home

--- src/content/index.ts
+++ src/content/index.ts
@@ -5,25 +5,28 @@ const main = () => {
   const videos = new GridVideos();
 
   document.addEventListener("keydown", function (event) {
-    // const path = window.location.pathname;
+    const path = window.location.pathname;
+
     // なにもハイライトされていないときに移動キーが押されたら最初の要素をハイライトする
-    if (movingKeys.includes(event.key) && !videos.highlighted()) {
-      videos.highlightFirst();
-      return;
-    }
-    switch (event.key) {
-      case moving.Left:
-        videos.highlightPrevious();
-        break;
-      case moving.Down:
-        videos.highlightDown();
-        break;
-      case moving.Up:
-        videos.highlightUp();
-        break;
-      case moving.Right:
-        videos.highlightNext();
-        break;
+    if (path === "/") {
+      if (movingKeys.includes(event.key) && !videos.highlighted()) {
+        videos.highlightFirst();
+        return;
+      }
+      switch (event.key) {
+        case moving.Left:
+          videos.highlightPrevious();
+          break;
+        case moving.Down:
+          videos.highlightDown();
+          break;
+        case moving.Up:
+          videos.highlightUp();
+          break;
+        case moving.Right:
+          videos.highlightNext();
+          break;
+      }
     }
   });
 };
