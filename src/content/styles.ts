826d4022453b2f26cc7668411c3e1dbe63b93f9d
EkeMinusYou <euonymuslke@gmail.com>
2024-06-15 17:23:08 +0900

    fix style

--- src/content/styles.ts
+++ src/content/styles.ts
@@ -2,7 +2,7 @@ import { css } from "@emotion/css";
 
 export const styles = {
   highlight: css({
-    border: "4px solid red",
+    outline: "4px solid red",
     boxSizing: "border-box",
   }),
 };
