bdabad2bde322a0533b07bd81945bb8b2405e0f6
EkeMinusYou <euonymuslke@gmail.com>
2024-10-24 00:58:30 +0900

    rename product

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { crx, defineManifest } from "@crxjs/vite-plugin";
 
 const manifest = defineManifest({
   manifest_version: 3,
-  name: "Shortcuts for Youtube",
+  name: "Better Youtube Life",
   version: "0.0.1",
   content_scripts: [
     {
